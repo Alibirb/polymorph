e55a51a40a1d2141eefc769595ee13e26be57db7
WriterOfAlicrow <dpiros@comcast.net>
2015-07-21 20:49:30 -0400

    Added squirreliness slider

--- morph.js
+++ morph.js
@@ -21,10 +21,11 @@ var template = Snap("#template_svg");
 var result = Snap("#result");
 var base_template;
 var cat_template;
+var squirrel_template;
 var template_groups;
 var loaded;
 var result_elements;
-var furriness, antifurriness;
+var furriness, squirreliness;
 
 
 function convertToAbsolute(path){
@@ -61,12 +62,13 @@ function convertToAbsolute(path){
 
 function refresh_display() {
 	furriness = document.getElementById("furriness_slider").value;
-	antifurriness = 1.0 - furriness;
+	squirreliness = document.getElementById("squirreliness_slider").value;
 	
 	result.clear();
 	
 	var base_elements = base_template.node.children;
 	var cat_elements = cat_template.node.children;
+	var squirrel_elements = squirrel_template.node.children;
 	result_elements = result.group();
 	
 	result_elements.attr({
@@ -79,11 +81,13 @@ function refresh_display() {
 	for(var i=0; i < base_elements.length; ++i) {
 		var base_e = base_elements[i];
 		var cat_e = cat_elements[i];
+		var squirrel_e = squirrel_elements[i];
 		if(base_e.nodeName !== "path")
 			continue;
 			
 		convertToAbsolute(base_e);
 		convertToAbsolute(cat_e);
+		convertToAbsolute(squirrel_e);
 		
 		var result_path = result_elements.path();
 		var result_e = result_path.node;
@@ -95,15 +99,20 @@ function refresh_display() {
 		/// combine the path properties.
 		var base_seglist = base_e.pathSegList;
 		var cat_seglist = cat_e.pathSegList;
+		var squirrel_seglist = squirrel_e.pathSegList;
 		var result_seglist = result_e.pathSegList;
 		
 		for(var j=0; j < base_seglist.length; ++j) {
 			/// segment in path
 			var base_seg = base_seglist[j];
 			var cat_seg = cat_seglist[j];
+			var squirrel_seg = squirrel_seglist[j];
 			var result_seg;
 			if(base_seg.pathSegType != cat_seg.pathSegType) {
 				console.warn("path segment types do not match: %i (%s), %i (%s)", base_seg.pathSegType, base_seg.pathSegTypeAsLetter, cat_seg.pathSegType, cat_seg.pathSegTypeAsLetter);
+			}
+			if(base_seg.pathSegType != squirrel_seg.pathSegType) {
+				console.warn("path segment types do not match: %i (%s), %i (%s)", base_seg.pathSegType, base_seg.pathSegTypeAsLetter, squirrel_seg.pathSegType, squirrel_seg.pathSegTypeAsLetter);
 			}
 			switch(base_seg.pathSegType) {
 				case SVGPathSeg.PATHSEG_CLOSEPATH: {
@@ -111,57 +120,57 @@ function refresh_display() {
 					break;
 				}
 				case SVGPathSeg.PATHSEG_MOVETO_ABS: {
-					var x = (base_seg.x * antifurriness + cat_seg.x * furriness);
-					var y = (base_seg.y * antifurriness + cat_seg.y * furriness);
+					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
+					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
 					result_seg = result_e.createSVGPathSegMovetoAbs(x,y);
 					break;
 				}
 				case SVGPathSeg.PATHSEG_LINETO_ABS: {
-					var x = (base_seg.x * antifurriness + cat_seg.x * furriness);
-					var y = (base_seg.y * antifurriness + cat_seg.y * furriness);
+					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
+					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
 					result_seg = result_e.createSVGPathSegLinetoAbs(x,y);
 					break;
 				}
 				case SVGPathSeg.PATHSEG_CURVETO_CUBIC_ABS: {
-					var x = (base_seg.x * antifurriness + cat_seg.x * furriness);
-					var y = (base_seg.y * antifurriness + cat_seg.y * furriness);
-					var x1 = (base_seg.x1 * antifurriness + cat_seg.x1 * furriness);
-					var y1 = (base_seg.y1 * antifurriness + cat_seg.y1 * furriness);
-					var x2 = (base_seg.x2 * antifurriness + cat_seg.x2 * furriness);
-					var y2 = (base_seg.y2 * antifurriness + cat_seg.y2 * furriness);
+					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
+					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
+					var x1 = base_seg.x1 + ((cat_seg.x1 - base_seg.x1) * furriness) + ((squirrel_seg.x1 - base_seg.x1) * squirreliness);
+					var y1 = base_seg.y1 + ((cat_seg.y1 - base_seg.y1) * furriness) + ((squirrel_seg.y1 - base_seg.y1) * squirreliness);
+					var x2 = base_seg.x2 + ((cat_seg.x2 - base_seg.x2) * furriness) + ((squirrel_seg.x2 - base_seg.x2) * squirreliness);
+					var y2 = base_seg.y2 + ((cat_seg.y2 - base_seg.y2) * furriness) + ((squirrel_seg.y2 - base_seg.y2) * squirreliness);
 					result_seg = result_e.createSVGPathSegCurvetoCubicAbs(x, y, x1, y1, x2, y2);
 					break;
 				}
 				case SVGPathSeg.PATHSEG_CURVETO_CUBIC_SMOOTH_ABS: {
-					var x = (base_seg.x * antifurriness + cat_seg.x * furriness);
-					var y = (base_seg.y * antifurriness + cat_seg.y * furriness);
-					var x2 = (base_seg.x2 * antifurriness + cat_seg.x2 * furriness);
-					var y2 = (base_seg.y2 * antifurriness + cat_seg.y2 * furriness);
+					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
+					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
+					var x2 = base_seg.x2 + ((cat_seg.x2 - base_seg.x2) * furriness) + ((squirrel_seg.x2 - base_seg.x2) * squirreliness);
+					var y2 = base_seg.y2 + ((cat_seg.y2 - base_seg.y2) * furriness) + ((squirrel_seg.y2 - base_seg.y2) * squirreliness);
 					result_seg = result_e.createSVGPathSegCurvetoCubicSmoothAbs(x, y, x2, y2);
 					break;
 				}
 				case SVGPathSeg.PATHSEG_CURVETO_QUADRATIC_ABS: {
-					var x = (base_seg.x * antifurriness + cat_seg.x * furriness);
-					var y = (base_seg.y * antifurriness + cat_seg.y * furriness);
-					var x1 = (base_seg.x1 * antifurriness + cat_seg.x1 * furriness);
-					var y1 = (base_seg.y1 * antifurriness + cat_seg.y1 * furriness);
+					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
+					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
+					var x1 = base_seg.x1 + ((cat_seg.x1 - base_seg.x1) * furriness) + ((squirrel_seg.x1 - base_seg.x1) * squirreliness);
+					var y1 = base_seg.y1 + ((cat_seg.y1 - base_seg.y1) * furriness) + ((squirrel_seg.y1 - base_seg.y1) * squirreliness);
 					result_seg = result_e.createSVGPathSegCurvetoQuadraticAbs(x, y, x1, y1);
 					break;
 				}
 				case SVGPathSeg.PATHSEG_CURVETO_QUADRATIC_SMOOTH_ABS: {
-					var x = (base_seg.x * antifurriness + cat_seg.x * furriness);
-					var y = (base_seg.y * antifurriness + cat_seg.y * furriness);
+					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
+					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
 					result_seg = result_e.createSVGPathSegCurvetoQuadraticSmoothAbs(x, y);
 					break;
 				}
 				case SVGPathSeg.PATHSEG_ARC_ABS: {
-					var x = (base_seg.x * antifurriness + cat_seg.x * furriness);
-					var y = (base_seg.y * antifurriness + cat_seg.y * furriness);
-					var r1 = (base_seg.r1 * antifurriness + cat_seg.r1 * furriness);
-					var r2 = (base_seg.r2 * antifurriness + cat_seg.r2 * furriness);
-					var angle = (base_seg.angle * antifurriness + cat_seg.angle * furriness);
-					var largeArcFlag = base_seg.largeArcFlag;	///FIXME
-					var sweepFlag = base_seg.sweepFlag;		///FIXME
+					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
+					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
+					var r1 = base_seg.r1 + ((cat_seg.r1 - base_seg.r1) * furriness) + ((squirrel_seg.r1 - base_seg.r1) * squirreliness);
+					var r2 = base_seg.r2 + ((cat_seg.r2 - base_seg.r2) * furriness) + ((squirrel_seg.r2 - base_seg.r2) * squirreliness);
+					var angle = base_seg.angle + ((cat_seg.angle - base_seg.angle) * furriness) + ((squirrel_seg.angle - base_seg.angle) * squirreliness);
+					var largeArcFlag = base_seg.largeArcFlag;	///FIXME: figure out how to blend this
+					var sweepFlag = base_seg.sweepFlag;		///FIXME: figure out how to blend this
 					result_seg = result_e.createSVGPathSegArcAbs(x, y, r1, r2, angle, largeArcFlag, sweepFlag);
 					break;
 				}
@@ -186,6 +195,7 @@ Snap.load("polymorph-opt.svg", function(loaded_svg) {
 	template_groups = loaded_svg.selectAll("g");
 	base_template = template_groups[0];
 	cat_template = template_groups[1];
+	squirrel_template = template_groups[2];
 	loaded = loaded_svg;
 	
 	refresh_display();
