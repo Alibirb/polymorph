feddeb6bbf422ce634ec298ee068e61eab86a590
WriterOfAlicrow <dpiros@comcast.net>
2015-09-05 23:53:14 -0400

    Refactoring

--- morph.js
+++ morph.js
@@ -24,8 +24,18 @@ var cat_template;
 var squirrel_template;
 var template_groups;
 var loaded;
-var result_elements;
+var result_elements_group;
 var furriness, squirreliness;
+
+
+function copy_attributes(src_node, dest_node) {
+	for(var i=0; i < src_node.attributes.length; ++i) {
+		var attr = src_node.attributes[i];
+		if(attr.name === "id" || attr.name === "d")
+			continue;
+		dest_node.setAttribute(attr.name, attr.value);
+	}
+}
 
 
 function convertToAbsolute(path){
@@ -60,6 +70,201 @@ function convertToAbsolute(path){
 	}
 }
 
+
+function merge_path_elements(result_path, elements, values) {
+	console.log("merging path elements");
+	
+	var seg_lists = [];
+	
+	for(var i=0; i < elements.length; ++i) {
+		var element = elements[i];
+		
+		if(element.nodeName !== "path") {
+			console.warn("element nodeName is '" + element.nodeName + "', not 'path'. ");
+			return;
+		}
+		
+		convertToAbsolute(element);
+		seg_lists[i] = element.pathSegList;
+	}
+	
+	var result_e = result_path.node;
+	
+	/// FIXME: morph these attributes
+	copy_attributes(elements[0], result_e);
+	
+	var base_seglist = seg_lists[0];
+	var result_seglist = result_e.pathSegList;
+	
+	if(elements.length === 1) {
+		/// Only one element to merge.
+		console.log("merging single element. Copying path directly");
+		result_e.setAttribute("d", elements[0].getAttribute("d"));
+		return;
+	}
+	
+	for(var i=0; i < base_seglist.length; ++i) {
+		/// segment in path
+		var base_seg = base_seglist[i];
+		var segments = [];
+		for(var j=0; j < elements.length; ++j) {
+			segments[j] = seg_lists[j][i];
+			
+			if(base_seg.pathSegType != segments[j].pathSegType) {
+				console.warn("path segment types do not match: %i (%s), %i (%s)", base_seg.pathSegType, base_seg.pathSegTypeAsLetter, segments[j].pathSegType, segment[j].pathSegTypeAsLetter);
+			}
+		}
+		var result_seg;
+		switch(base_seg.pathSegType) {
+			case SVGPathSeg.PATHSEG_CLOSEPATH: {
+				result_seg = result_e.createSVGPathSegClosePath();
+				break;
+			}
+			case SVGPathSeg.PATHSEG_MOVETO_ABS: {
+				var x = base_seg.x;
+				var y = base_seg.y;
+				for(var j=1; j < elements.length; ++j) {
+					x += (segments[j].x - base_seg.x) * values[j];
+					y += (segments[j].y - base_seg.y) * values[j];
+				}
+				result_seg = result_e.createSVGPathSegMovetoAbs(x,y);
+				break;
+			}
+			case SVGPathSeg.PATHSEG_LINETO_ABS: {
+				var x = base_seg.x;
+				var y = base_seg.y;
+				for(var j=1; j < elements.length; ++j) {
+					x += (segments[j].x - base_seg.x) * values[j];
+					y += (segments[j].y - base_seg.y) * values[j];
+				}
+				result_seg = result_e.createSVGPathSegLinetoAbs(x,y);
+				break;
+			}
+			case SVGPathSeg.PATHSEG_CURVETO_CUBIC_ABS: {
+				var x = base_seg.x;
+				var y = base_seg.y;
+				var x1 = base_seg.x1;
+				var y1 = base_seg.y1;
+				var x2 = base_seg.x2;
+				var y2 = base_seg.y2;
+				for(var j=1; j < elements.length; ++j) {
+					x += (segments[j].x - base_seg.x) * values[j];
+					y += (segments[j].y - base_seg.y) * values[j];
+					x1 += (segments[j].x1 - base_seg.x1) * values[j];
+					y1 += (segments[j].y1 - base_seg.y1) * values[j];
+					x2 += (segments[j].x2 - base_seg.x2) * values[j];
+					y2 += (segments[j].y2 - base_seg.y2) * values[j];
+				}
+				result_seg = result_e.createSVGPathSegCurvetoCubicAbs(x, y, x1, y1, x2, y2);
+				break;
+			}
+			case SVGPathSeg.PATHSEG_CURVETO_CUBIC_SMOOTH_ABS: {
+				var x = base_seg.x;
+				var y = base_seg.y;
+				var x2 = base_seg.x2;
+				var y2 = base_seg.y2;
+				for(var j=1; j < elements.length; ++j) {
+					x += (segments[j].x - base_seg.x) * values[j];
+					y += (segments[j].y - base_seg.y) * values[j];
+					x2 += (segments[j].x2 - base_seg.x2) * values[j];
+					y2 += (segments[j].y2 - base_seg.y2) * values[j];
+				}
+				result_seg = result_e.createSVGPathSegCurvetoCubicSmoothAbs(x, y, x2, y2);
+				break;
+			}
+			case SVGPathSeg.PATHSEG_CURVETO_QUADRATIC_ABS: {
+				var x = base_seg.x;
+				var y = base_seg.y;
+				var x1 = base_seg.x1;
+				var y1 = base_seg.y1;
+				for(var j=1; j < elements.length; ++j) {
+					x += (segments[j].x - base_seg.x) * values[j];
+					y += (segments[j].y - base_seg.y) * values[j];
+					x1 += (segments[j].x1 - base_seg.x1) * values[j];
+					y1 += (segments[j].y1 - base_seg.y1) * values[j];
+				}
+				result_seg = result_e.createSVGPathSegCurvetoQuadraticAbs(x, y, x1, y1);
+				break;
+			}
+			case SVGPathSeg.PATHSEG_CURVETO_QUADRATIC_SMOOTH_ABS: {
+				var x = base_seg.x;
+				var y = base_seg.y;
+				for(var j=1; j < elements.length; ++j) {
+					x += (segments[j].x - base_seg.x) * values[j];
+					y += (segments[j].y - base_seg.y) * values[j];
+				}
+				result_seg = result_e.createSVGPathSegCurvetoQuadraticSmoothAbs(x, y);
+				break;
+			}
+			case SVGPathSeg.PATHSEG_ARC_ABS: {
+				var largeArcFlag = base_seg.largeArcFlag;	///FIXME: figure out how to blend this
+				var sweepFlag = base_seg.sweepFlag;		///FIXME: figure out how to blend this
+				var x = base_seg.x;
+				var y = base_seg.y;
+				var r1 = base_seg.r1;
+				var r2 = base_seg.r2;
+				var angle = base_seg.angle;
+				for(var j=1; j < elements.length; ++j) {
+					x += (segments[j].x - base_seg.x) * values[j];
+					y += (segments[j].y - base_seg.y) * values[j];
+					r1 += (segments[j].r1 - base_seg.r1) * values[j];
+					r2 += (segments[j].r2 - base_seg.r2) * values[j];
+					angle += (segments[j].angle - base_seg.angle) * values[j];
+				}
+				result_seg = result_e.createSVGPathSegArcAbs(x, y, r1, r2, angle, largeArcFlag, sweepFlag);
+				break;
+			}
+			default: {
+				console.error("no morph implementation for path segment type %s", base_seg.pathSegTypeAsLetter);
+				break;
+			}
+		}
+		if(result_seg != null)
+			result_seglist.appendItem(result_seg);
+	}
+}
+
+function merge_group_elements(result_group, groups, values) {
+	console.log("merging group elements");
+
+	var base_group = groups[0];
+	var base_elements = base_group.children;
+	
+	/// Copy base attributes into result group
+	copy_attributes(base_group, result_group.node);
+	
+	for(var i=0; i < base_elements.length; ++i) {
+		var base_e = base_elements[i];
+		var child_elements = [];
+		for(var j=0; j < groups.length; ++j) {
+			child_elements[j] = groups[j].children[i];
+		}
+		
+		if(base_e.nodeName === "path") {
+			merge_path_elements(result_group.path(), child_elements, values);
+		} else if(base_e.nodeName === "g") {
+			var group_name = base_e.getAttribute("polymorph:label");
+			
+			/// find matching nodes in the other groups
+			var matching_groups = [];
+			var matching_group_values = [];
+			for(var j=0; j < groups.length; ++j) {
+				for(var k=0; k < groups[j].children.length; ++k) {
+					if(groups[j].children[k].nodeName === "g" && groups[j].children[k].getAttribute("polymorph:label") === group_name) {
+						matching_groups.push(groups[j].children[k]);
+						matching_group_values.push(values[j]);
+						break;
+					}
+				}
+			}
+			merge_group_elements(result_group.group(), matching_groups, matching_group_values);
+		} else {
+			console.warn("nodeName is '" + base_e.nodeName + "', not 'path' or 'g'.");
+		}
+	}
+}
+
+
 function refresh_display() {
 	furriness = document.getElementById("furriness_slider").value;
 	squirreliness = document.getElementById("squirreliness_slider").value;
@@ -69,121 +274,20 @@ function refresh_display() {
 	var base_elements = base_template.node.children;
 	var cat_elements = cat_template.node.children;
 	var squirrel_elements = squirrel_template.node.children;
-	result_elements = result.group();
 	
-	result_elements.attr({
-		stroke: base_template.node.getAttribute("stroke"),
-		fill: base_template.node.getAttribute("fill"),
-		"stroke-linecap": base_template.node.getAttribute("stroke-linecap"),
-		transform: base_template.node.getAttribute("transform")
-	});
+	/// result_elements_group is analogous to base_template
+	result_elements_group = result.group();
 	
-	for(var i=0; i < base_elements.length; ++i) {
-		var base_e = base_elements[i];
-		var cat_e = cat_elements[i];
-		var squirrel_e = squirrel_elements[i];
-		if(base_e.nodeName !== "path")
+	/// Copy base attributes into result group
+	for(var i=0; i < base_template.node.attributes.length; ++i) {
+		var attr = base_template.node.attributes[i];
+		if(attr.name === "id") {
 			continue;
-			
-		convertToAbsolute(base_e);
-		convertToAbsolute(cat_e);
-		convertToAbsolute(squirrel_e);
-		
-		var result_path = result_elements.path();
-		var result_e = result_path.node;
-		/// FIXME: morph transformation.
-		result_path.attr({
-			transform: base_e.getAttribute("transform")
-		});
-		
-		/// combine the path properties.
-		var base_seglist = base_e.pathSegList;
-		var cat_seglist = cat_e.pathSegList;
-		var squirrel_seglist = squirrel_e.pathSegList;
-		var result_seglist = result_e.pathSegList;
-		
-		for(var j=0; j < base_seglist.length; ++j) {
-			/// segment in path
-			var base_seg = base_seglist[j];
-			var cat_seg = cat_seglist[j];
-			var squirrel_seg = squirrel_seglist[j];
-			var result_seg;
-			if(base_seg.pathSegType != cat_seg.pathSegType) {
-				console.warn("path segment types do not match: %i (%s), %i (%s)", base_seg.pathSegType, base_seg.pathSegTypeAsLetter, cat_seg.pathSegType, cat_seg.pathSegTypeAsLetter);
-			}
-			if(base_seg.pathSegType != squirrel_seg.pathSegType) {
-				console.warn("path segment types do not match: %i (%s), %i (%s)", base_seg.pathSegType, base_seg.pathSegTypeAsLetter, squirrel_seg.pathSegType, squirrel_seg.pathSegTypeAsLetter);
-			}
-			switch(base_seg.pathSegType) {
-				case SVGPathSeg.PATHSEG_CLOSEPATH: {
-					result_seg = result_e.createSVGPathSegClosePath();
-					break;
-				}
-				case SVGPathSeg.PATHSEG_MOVETO_ABS: {
-					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
-					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
-					result_seg = result_e.createSVGPathSegMovetoAbs(x,y);
-					break;
-				}
-				case SVGPathSeg.PATHSEG_LINETO_ABS: {
-					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
-					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
-					result_seg = result_e.createSVGPathSegLinetoAbs(x,y);
-					break;
-				}
-				case SVGPathSeg.PATHSEG_CURVETO_CUBIC_ABS: {
-					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
-					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
-					var x1 = base_seg.x1 + ((cat_seg.x1 - base_seg.x1) * furriness) + ((squirrel_seg.x1 - base_seg.x1) * squirreliness);
-					var y1 = base_seg.y1 + ((cat_seg.y1 - base_seg.y1) * furriness) + ((squirrel_seg.y1 - base_seg.y1) * squirreliness);
-					var x2 = base_seg.x2 + ((cat_seg.x2 - base_seg.x2) * furriness) + ((squirrel_seg.x2 - base_seg.x2) * squirreliness);
-					var y2 = base_seg.y2 + ((cat_seg.y2 - base_seg.y2) * furriness) + ((squirrel_seg.y2 - base_seg.y2) * squirreliness);
-					result_seg = result_e.createSVGPathSegCurvetoCubicAbs(x, y, x1, y1, x2, y2);
-					break;
-				}
-				case SVGPathSeg.PATHSEG_CURVETO_CUBIC_SMOOTH_ABS: {
-					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
-					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
-					var x2 = base_seg.x2 + ((cat_seg.x2 - base_seg.x2) * furriness) + ((squirrel_seg.x2 - base_seg.x2) * squirreliness);
-					var y2 = base_seg.y2 + ((cat_seg.y2 - base_seg.y2) * furriness) + ((squirrel_seg.y2 - base_seg.y2) * squirreliness);
-					result_seg = result_e.createSVGPathSegCurvetoCubicSmoothAbs(x, y, x2, y2);
-					break;
-				}
-				case SVGPathSeg.PATHSEG_CURVETO_QUADRATIC_ABS: {
-					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
-					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
-					var x1 = base_seg.x1 + ((cat_seg.x1 - base_seg.x1) * furriness) + ((squirrel_seg.x1 - base_seg.x1) * squirreliness);
-					var y1 = base_seg.y1 + ((cat_seg.y1 - base_seg.y1) * furriness) + ((squirrel_seg.y1 - base_seg.y1) * squirreliness);
-					result_seg = result_e.createSVGPathSegCurvetoQuadraticAbs(x, y, x1, y1);
-					break;
-				}
-				case SVGPathSeg.PATHSEG_CURVETO_QUADRATIC_SMOOTH_ABS: {
-					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
-					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
-					result_seg = result_e.createSVGPathSegCurvetoQuadraticSmoothAbs(x, y);
-					break;
-				}
-				case SVGPathSeg.PATHSEG_ARC_ABS: {
-					var x = base_seg.x + ((cat_seg.x - base_seg.x) * furriness) + ((squirrel_seg.x - base_seg.x) * squirreliness);
-					var y = base_seg.y + ((cat_seg.y - base_seg.y) * furriness) + ((squirrel_seg.y - base_seg.y) * squirreliness);
-					var r1 = base_seg.r1 + ((cat_seg.r1 - base_seg.r1) * furriness) + ((squirrel_seg.r1 - base_seg.r1) * squirreliness);
-					var r2 = base_seg.r2 + ((cat_seg.r2 - base_seg.r2) * furriness) + ((squirrel_seg.r2 - base_seg.r2) * squirreliness);
-					var angle = base_seg.angle + ((cat_seg.angle - base_seg.angle) * furriness) + ((squirrel_seg.angle - base_seg.angle) * squirreliness);
-					var largeArcFlag = base_seg.largeArcFlag;	///FIXME: figure out how to blend this
-					var sweepFlag = base_seg.sweepFlag;		///FIXME: figure out how to blend this
-					result_seg = result_e.createSVGPathSegArcAbs(x, y, r1, r2, angle, largeArcFlag, sweepFlag);
-					break;
-				}
-				default: {
-					console.error("no morph implementation for path segment type %s", base_seg.pathSegTypeAsLetter);
-					break;
-				}
-			}
-			if(result_seg != null)
-				result_seglist.appendItem(result_seg);
 		}
+		result_elements_group.node.setAttribute(attr.name, attr.value);
 	}
 	
+	merge_group_elements(result_elements_group, [base_template.node, cat_template.node, squirrel_template.node], [1, furriness, squirreliness]);
 }
 
 
@@ -192,7 +296,7 @@ Snap.load("polymorph-opt.svg", function(loaded_svg) {
 		height: 500,
 		width: 200
 	});
-	template_groups = loaded_svg.selectAll("g");
+	template_groups = loaded_svg.selectAll("svg > g");	/// all g elements whose parent is an svg element
 	base_template = template_groups[0];
 	cat_template = template_groups[1];
 	squirrel_template = template_groups[2];
